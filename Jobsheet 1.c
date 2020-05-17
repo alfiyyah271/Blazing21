9a30bbc656e8598343e90b71610e689fd346abe8
alfiyyah reghina larasati <64623304+alfiyyah271@users.noreply.github.com>
2020-05-17 23:32:17 +0800

    Create Jobsheet 1.c

---
+++ Jobsheet 1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+main (){
+int nim[12]= {11,7,8,7,1};
+printf("Nama  = Alfiyyah Reghina Larasati\n");
+printf("NIM  = F1B019022\n");
+printf("Kelompok = 2\n\n");
+printf ("Nim ke-1 = %i\n",nim[0]);
+printf ("Nim ke-2 = %i\n",nim[1]);
+printf ("Nim ke-3 = %i\n",nim[2]);
+printf ("Nim ke-4 = %i\n",nim[3]);
+printf ("Nim ke-5 = %i\n",nim[4]);
+}
