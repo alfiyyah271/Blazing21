f748643163155ceba3e404cbce69b9d8c62769df
alfiyyah reghina larasati <64623304+alfiyyah271@users.noreply.github.com>
2020-05-17 23:31:42 +0800

    Create Jobsheet 1.cpp

---
+++ Jobsheet 1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<"Nama : alfiyyah reghina larasati\nNIM : F1B019022\nKelompok : 2\n\n\n";
+	int nim[12]= {1,9,0,2,2};
+	
+	cout << "Nim ke-1 adalah " << nim[0] << endl;
+	cout << "Nim ke-2 adalah " << nim[1] << endl;
+	cout << "Nim ke-3 adalah " << nim[2] << endl;
+	cout << "Nim ke-4 adalah " << nim[3] << endl;
+	cout << "Nim ke-5 adalah " << nim[4] << endl;
+
+	
+
+}
+	
